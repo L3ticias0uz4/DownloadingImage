4db530b1eb6d006c7b91a5ff1a58c77816ff8f7a
L3ticias0uz4 <leticiasouza56000@gmail.com>
2026-05-13 10:42:42 -0300

    Create User Repository

---
+++ src/main/java/com/example/foto/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.foto.repositories;
+
+import com.example.foto.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
